016651b7f13bd8a28563158514a404249e96dfb8
gholder <gaylord.holder@gmail.com>
2020-02-05 12:20:56 -0500

    working startup script

--- bin/slurm-startup-script.sh
+++ bin/slurm-startup-script.sh
@@ -4,15 +4,15 @@ function getMetadata {
 	curl -s -H "Metadata-Flavor:Google" "http://metadata.google.internal/computeMetadata/v1/instance/$1"
 }
 
-HostName=$(getMetadata MinionID)
 
 if [ ! -d /etc/salt ]
 then
 	mkdir /etc/salt
-	echo "$HostName" > /etc/salt/minion_id
+	MinionID=$(getMetadata attributes/MinionID)
 
-        curl -L https://bootstrap.saltstack.com -o /tmp/install_salt.sh
-        sudo sh /tmp/install_salt.sh git 
+	echo "$MinionID" > /etc/salt/minion_id
+
+	curl -L https://bootstrap.saltstack.com | sudo sh -s -- git develop
 
 	sed -i '/^#master:.*$/s//master: salt.cfn.upenn.edu/' /etc/salt/minion
 	sed -i '/^#hash_type.*$:/s//hash_type: sha256/' /etc/salt/minion
